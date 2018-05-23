acd47e7879028dc456277be04222f9d2af895091
rahulrainaaa <rahulrainahome@live.com>
2018-05-23 15:41:22 +0530

    Initial Commit.

---
+++ GISTest/GeometryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GeometryViewController.swift
+//  GISTest
+//
+//  Created by Mobility on 5/23/18.
+//  Copyright © 2018 Mobility. All rights reserved.
+//
+
+import UIKit
+
+class GeometryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+    }
+    
+}
